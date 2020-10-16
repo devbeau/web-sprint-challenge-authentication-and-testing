051a5e914f9b6ae9f4338102fe898af910b6bdad
Beau Bradley <beaubradley@outlook.com>
2020-10-16 12:45:18 -0400

    init commit

--- api/server.js
+++ api/server.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const authenticate = require('../auth/authenticate-middleware');
 
 const server = express();
+
+const sessionConfiguration = {
+    name: "sid",
+    secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
+    cookie: {
+        httpOnly: true, 
+        maxAge: 1000 * 60 * 10, 
+        secure: process.env.SECURE_COOKIES || false,
+    },
+    resave: false, 
+    saveUninitialized: true, 
+    store: new KnexSessionStore({
+        knex: connection, 
+        tablename: "sessions",
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 1000 * 60 * 60, 
+    }),
+};
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfiguration));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,45 @@
 const router = require('express').Router();
+const bcryptjs = require('bcryptjs');
+const Users = require('../users/usersModel');
 
 router.post('/register', (req, res) => {
   // implement registration
+  const creds = req.body;
+
+  const rounds = Number(process.env.HASH_ROUNDS) || 6);
+  const hash = bcryptjs.hashSync(creds.password, rounds);
+
+  creds.password = hash;
+
+  Users.add(creds)
+    .then(user => {
+      res.status(200).json({data: user});
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message});
+    })
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  const creds = req.body;
+
+  Users.findBy({username: creds.username})
+    .then(users => {
+      const user = users[0];
+
+      if (user && bcryptjs.compareSync(creds.password, user.password)){
+        req.session.username = user.username;
+
+        res.status(200).json({
+          message: 'welcome',
+          username: req.username
+        });
+      } else {
+        res.status(401).json({message: 'Invalid Credentials'})
+      }
+    })
+    .catch(err => res.status(500).json({message: 'Invalid Credentials'}));
 });
 
 module.exports = router;
